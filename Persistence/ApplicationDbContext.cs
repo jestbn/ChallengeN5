c134b174dc94441fbe075399e379294c7b3bfe5d
Esteban <juanestebang9@gmail.com>
2023-11-23 20:52:10 -0500

    database creation and use assurance

--- Persistence/ApplicationDbContext.cs
+++ Persistence/ApplicationDbContext.cs
@@ -2,12 +2,34 @@
 using Domain.Permisos;
 using Domain.TipoPermiso;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Persistence
 {
     public sealed class ApplicationDbContext : DbContext, IUnitOfWork
     {
-        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            try
+            {
+                var databasecreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if (databasecreator != null)
+                {
+                    if (!databasecreator.CanConnect())
+                    {
+                        databasecreator.Create();
+
+                    }
+                    //if (!databasecreator.HasTables()) databasecreator.CreateTables();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
         public DbSet<Permiso> Permisos { get; set; }
         public DbSet<TipoPermiso> TipoPermisos { get; set; }
 
