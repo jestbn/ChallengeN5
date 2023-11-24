9f52b6de02649c7fe8548000edb04887f4c94a18
Esteban <juanestebang9@gmail.com>
2023-11-24 12:21:06 -0500

    removed extra using

--- Application.Tests/BehaviourTests/ExternalBehaviourTests.cs
+++ Application.Tests/BehaviourTests/ExternalBehaviourTests.cs
@@ -3,7 +3,6 @@ using Application.Permisos.Create;
 using Domain.Shared;
 using Infraestructure.ElasticSearch;
 using Infraestructure.Kafka;
-using Infraestructure.LogWrapper;
 using MediatR;
 
 namespace Application.Tests.BehaviourTests;
